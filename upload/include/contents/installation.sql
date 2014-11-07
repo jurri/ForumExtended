437875e32196e010c5e806b3c23fc0a9a95e9ddb
jurri <juri.derewjanko@buw.de>
2014-11-07 12:38:31 +0100

    1.6.4
    install.sql angepasst für die erweiterte Administration der Rechte :)

--- upload/include/contents/installation.sql
+++ upload/include/contents/installation.sql
@@ -2,7 +2,7 @@
 -- Tabellenstruktur für Tabelle `ic1_danke`
 --
 
-CREATE TABLE `ic1_danke` (
+CREATE TABLE `prefix_danke` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `erstid` int(11) NOT NULL,
   `erstname` varchar(30) NOT NULL,
@@ -11,4 +11,15 @@ CREATE TABLE `ic1_danke` (
   `tid` int(11) NOT NULL,
   `pid` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1 COMMENT='sag Danke by GeCk0 mod by FeTTsack'
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1 COMMENT='sag Danke by GeCk0';
+
+CREATE TABLE `prefix_forumrecht` (
+  `recht_pk` int(11) NOT NULL AUTO_INCREMENT,
+  `recht_fkforum` int(11) NOT NULL,
+  `recht_fkrecht` int(11) NOT NULL,
+  `recht_bearbeiten` int(11) NOT NULL,
+  PRIMARY KEY (`recht_pk`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+ALTER TABLE `prefix_forums`
+ADD COLUMN `erwrecht` tinyint(1) NOT NULL AFTER `besch`;
